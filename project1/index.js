c321adbd1bbddfaa7068872898a20a0c04261d36
Prem Vispute <Premvispute@gmail.com>
2021-09-25 12:43:44 +0530

    First Commit

---
+++ project1/index.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementsByClassName("sidenav")[0].style.width = "100%";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementsByClassName("sidenav")[0].style.width = "0";
+  }
